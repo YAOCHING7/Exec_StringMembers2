f19898423ca0c72eb64692cd21c222a40ed6aaff
Tim <vunvun0213@gmail.com>
2022-10-20 01:13:17 +0800

    加入專案檔案。

---
+++ Q3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "aLLeN KUO";
+            string nameResult = name.ToLower();
+            string nameResult2 = nameResult.Substring(0, 1);
+            string nameResult3 = nameResult.Substring(6, 1);
+            string nameResult4 = nameResult2.ToUpper();
+            string nameResult5 = nameResult3.ToUpper();
+            string nameResult6 = nameResult.Substring(1, 5);
+            string nameResult7 = nameResult.Substring(7, 2);
+            string nameResult8 = nameResult4 + nameResult6 + nameResult5 + nameResult7;
+            Console.WriteLine(nameResult8); //醜爛抱歉
+
+        }
+
+    }
+}
---
+++ Q6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入英文Allen:");
+            string name = Console.ReadLine();
+            string s1 = "Allen";
+
+            bool result = name.Equals(s1);
+            Console.WriteLine(result);
+        }
+    }
+}
